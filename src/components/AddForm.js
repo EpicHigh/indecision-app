8eb28c676e7eb53f83602fdb7bd16b1d98b86325
EpicHigh <prasit_tongpradit@windowslive.com>
2018-07-19 19:03:10 +0700

    Change some text;

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -3,10 +3,10 @@ import { AddOption, AddOptionInput, Button } from "../styles/main";
 
 export const AddForm = ({ submitForm }) => (
   <div>
-    <h3>Add your option:</h3>
+    <h3>Add your tasks:</h3>
     <AddOption onSubmit={submitForm}>
       <AddOptionInput type="text" name="option" required />
-      <Button type="submit">Add Option</Button>
+      <Button type="submit">Add A Task</Button>
     </AddOption>
   </div>
 );
